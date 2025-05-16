d749f9e6b7edf6361ab44f8bf8342400f27e7843
Jordan Micko Deloria <deloriamicko@gmail.com>
2025-05-17 01:06:45 +0800

    initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Feed from './Feed'  // import Feed component
+import Feed from './Feed'  
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Feed />  {/* Render the Feed component */}
+    <Feed />  {}
   </StrictMode>
 )
